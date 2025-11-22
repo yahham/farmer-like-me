c315e84134425d2786f5e8fee892856c8d9a8ed0
Yahia Hamitou <hamitou.yah@gmail.com>
2025-11-22 14:02:51 +0100

    update screens

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,17 +1,26 @@
 import { Stack } from "expo-router";
+import { Colors } from "../constants/Colors";
+import { useColorScheme } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme] ?? Colors.light;
+
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: { backgroundColor: "#ddd" },
-                headerTintColor: "#333",
-            }}
-        >
-            {/* Individual Screens */}
-            <Stack.Screen name="index" options={{ title: "Home" }} />
-            <Stack.Screen name="about" options={{ title: "About" }} />
-            <Stack.Screen name="contact" options={{ title: "Contact" }} />
-        </Stack>
+        <>
+            <StatusBar value="auto" />
+            <Stack
+                screenOptions={{
+                    headerStyle: { backgroundColor: theme.navBackground },
+                    headerTintColor: theme.title,
+                }}
+            >
+                {/* Individual Screens */}
+                <Stack.Screen name="index" options={{ title: "Home" }} />
+                <Stack.Screen name="about" options={{ title: "About" }} />
+                <Stack.Screen name="contact" options={{ title: "Contact" }} />
+            </Stack>
+        </>
     );
 }
--- app/about.jsx
+++ app/about.jsx
@@ -1,12 +1,18 @@
 import { Link } from "expo-router";
 import { View, Text, StyleSheet } from "react-native";
 
-const About = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>About Us</Text>
+import { Colors } from "../constants/Colors";
+import { useColorScheme } from "react-native";
 
-            <Link href="/" style={styles.link}>
+const About = () => {
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme] ?? Colors.light;
+
+    return (
+        <View style={[styles.container, { backgroundColor: theme.background }]}>
+            <Text style={[styles.title, { color: theme.title }]}>About Us</Text>
+
+            <Link href="/" style={[styles.link, { color: theme.text }]}>
                 Home
             </Link>
         </View>
