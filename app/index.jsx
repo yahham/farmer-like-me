98cd7a4af0da889043d6f5145b16577bdab6d21f
Yahia Hamitou <hamitou.yah@gmail.com>
2025-11-22 11:59:13 +0100

    update home screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import Logo from "../assets/images/flm-logo.png";
 
 const Home = () => {
     return (
-        <View>
-            <Text>FarmerLikeMe</Text>
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img} />
+
+            <Text style={styles.title}>Farmer Like Me</Text>
+
+            <Text style={{ marginTop: 10, marginBottom: 30 }}>
+                Growing Together
+            </Text>
         </View>
     );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#e0dfe8",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    img: {
+        marginVertical: 20,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+});
