fd524d5f92df9ab1f54b067fb5e07375e5387c30
Yahia Hamitou <hamitou.yah@gmail.com>
2025-11-22 16:59:43 +0100

    update layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -16,10 +16,8 @@ export default function RootLayout() {
                     headerTintColor: theme.title,
                 }}
             >
-                {/* Individual Screens */}
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
                 <Stack.Screen name="index" options={{ title: "Home" }} />
-                <Stack.Screen name="about" options={{ title: "About" }} />
-                <Stack.Screen name="contact" options={{ title: "Contact" }} />
             </Stack>
         </>
     );
--- app/index.jsx
+++ app/index.jsx
@@ -20,12 +20,12 @@ const Home = () => {
                 Growing Together
             </ThemedText>
 
-            <Link href="/about" style={styles.link}>
-                <ThemedText>About</ThemedText>
+            <Link href="/login" style={styles.link}>
+                <ThemedText>Login</ThemedText>
             </Link>
 
-            <Link href="/contact" style={styles.link}>
-                <ThemedText>Contact</ThemedText>
+            <Link href="/register" style={styles.link}>
+                <ThemedText>Register</ThemedText>
             </Link>
         </ThemedView>
     );
