ad3b3c280a668c432f54c66e288231c66857f99d
Yahia Hamitou <hamitou.yah@gmail.com>
2025-11-22 11:21:06 +0100

    update project structure for Expo Router

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>FarmerLikeMe</Text>
+        </View>
+    );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
