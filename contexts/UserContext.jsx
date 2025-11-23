1193b300ba6c21e197c64edba32dea3521652db7
Yahia Hamitou <hamitou.yah@gmail.com>
2025-11-23 16:44:23 +0100

    add contexts

---
+++ contexts/UserContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useEffect, useState } from "react";
+import { account } from "../lib/appwrite";
+import { ID } from "react-native-appwrite";
+
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [authChecked, setAuthChecked] = useState(false);
+
+    async function login(email, password) {
+        try {
+            await account.createEmailPasswordSession(email, password);
+            const response = await account.get();
+            setUser(response);
+        } catch (error) {
+            throw Error(error.message);
+        }
+    }
+
+    async function register(email, password) {
+        try {
+            await account.create(ID.unique(), email, password);
+            await login(email, password);
+        } catch (error) {
+            throw Error(error.message);
+        }
+    }
+
+    async function logout() {
+        await account.deleteSession("current");
+        setUser(null);
+    }
+
+    async function getInitialUserValue() {
+        try {
+            const res = await account.get();
+            setUser(res);
+        } catch (error) {
+            setUser(null);
+        } finally {
+            setAuthChecked(true);
+        }
+    }
+
+    useEffect(() => {
+        getInitialUserValue();
+    }, []);
+
+    return (
+        <UserContext.Provider
+            value={{
+                user,
+                login,
+                logout,
+                register,
+                authChecked,
+            }}
+        >
+            {children}
+        </UserContext.Provider>
+    );
+}
